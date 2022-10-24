18caa3639675c2afdaa338f7bb37cf0842307f98
Jakub Komárek <45208183+JakubKomarek@users.noreply.github.com>
2022-10-24 11:53:52 +0200

    Add files via upload

---
+++ R/testy/01_vzor_v1.R
@@ -0,0 +1,85 @@
+#####nainstaluji balíky, pokud ještě nemám
+install.packages("dplyr")
+install.packages("forcats")
+
+
+#####nahraji balíky
+library(dplyr)
+library(forcats)
+
+
+#####nahraji data (zkontroluj svou path)
+data <- readRDS("_RDS/01_data_pro_test.RDS")
+
+############co v datech mám?
+str(data)
+colnames(data)
+head(data)
+glimpse(data)
+nrow(data)  ###1286 středních škol
+
+###########################01 kolik škol, je v každém kraj?
+##najdu v dplyr cheat_sheet správnou funkci => count()
+
+data %>% 
+  count(kraj)
+
+##########################02 zmeňte sloupec kraj na faktor dle pořadí NUTS3. 
+##co za proměnnou je teď kraj?
+class(data$kraj)
+
+
+##je to „character vector" a my chceme factor (viz vámi vytvořené myšlenkové mapy, připomeň si faktory)
+
+##v dplyr měníme sloupce, jako bychom tvořili nové, takže funkce mutate()
+
+data <- data %>% 
+  mutate(kraj = "?????") ###co tam doplnit?
+           
+           
+####prozkoumejme funkci fct_relevel
+?fct_relevel()
+
+####vytvoříme zkušební vektor 
+vektor <- c("b", "b", "a")
+class(vektor) ##opět „character vektor"
+
+###chceme ho změnit na facktor, kde "a" je první hodnota a "b" je druhá hodnota
+new_vector <- fct_relevel(vector, "a", "b")
+
+levels(new_vector)
+##vida, "a" je první a lze "sortovat" (změnit pořadí dle pořadí faktorů)
+sort(new_vector) 
+
+##pojďme tedy zpátky k původnímu zadání
+data <- data %>% 
+  mutate(kraj = fct_relevel(kraj, c("Praha", "Středočeský", "Jihočeský", "Plzeňský", "Karlovarský",
+                                                       "Ústecký", "Liberecký","Královéhradecký", "Pardubický", "Vysočina", 
+                                                       "Jihomoravský", "Olomoucký", "Zlínský", "Moravskoslezský"))) %>% 
+           arrange(kraj)  ####seřadit dle pořadí faktor.
+
+
+data  ###první už jsou pražské školy
+
+##########################03 přiřaďte školy do správných NUTS2
+##mám 1286 SŠ, které potřebuji přiřadit do správných NUTS2
+
+##vytvořím dataframe, který použiji jako převodník
+
+prevodnik <- tibble(NUTS3 = c("Praha", "Středočeský", "Jihočeský", "Plzeňský", 
+                             "Karlovarský","Ústecký", "Liberecký","Královéhradecký", "Pardubický", 
+                             "Vysočina", "Jihomoravský", "Olomoucký", "Zlínský", "Moravskoslezský"),
+                    NUTS2 = c("CZ01", "CZ02", "CZ03", "CZ03", 
+                             "CZ04", "CZ04", "CZ05", "CZ05", "CZ05",
+                             "CZ06", "CZ06", "CZ07", "CZ07", "CZ08"))
+
+prevodnik
+
+
+##použiji klíč data$kraj <=> prevodnik$NUTS, abych přiřadil NUTS2
+
+data <- data %>% 
+  left_join(prevodnik, by = c("kraj" = "NUTS3"))
+
+
+data # a je to :)
